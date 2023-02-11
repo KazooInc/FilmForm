f94040c0503f54fbc4c5f4a0651528e85432997c
Raffaele Crocco <raffaelecrocco2000@gmail.com>
2023-02-11 22:29:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Landing from "./pages/Landing";
+import "./style/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Alert, Button } from "react-bootstrap";
+import Navigation from "../components/Navigation";
+
+const Landing = () => {
+  return (
+    <div className="Landing">
+      <Navigation />
+      <Container className="mt-5">testo</Container>
+    </div>
+  );
+};
+
+export default Landing;
