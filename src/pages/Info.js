fc9c351e2c270b56adec06b8697ba4d1f1fc2cd8
KazooInc <kazooaps@gmail.com>
2023-03-14 18:54:31 +0100

    Paginata Info

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -10,21 +10,38 @@ function Info() {
     <div className="Info">
       <Container >
         <Row className="py-5">
-          <Col>
-            <img src={logo} className="logo" />
-          </Col>
-          <Col xs="auto" className="sottotitolo">
-            Aiutaci a spargere la voce.
-          </Col>
+          
         </Row>
         <Row xs={1} md={2} className="py-5">
           <Col className="pe-5 mb-5">
-            <p className="titolo">Cinema a Povo</p>
-            <p className="sottotitolo">Autore e altre informazioni sull’anno</p>
+            <p className="titolo">Foglietto Illustrativo</p>
+            <p className="sottotitolo">Una proposta per cinefili e cinofili della città e della collina</p>
             <p className="testo">
-              Lorem ipsum dolor sit amet consectetur. Aliquet eget commodo
-              integer nulla magna cras. Dignissim cras varius ultricies velit
-              massa non id.
+              Passeggiando lungo la pedonale di Via dei Rivi (<a href="https://it.wikipedia.org/wiki/Povo" target="_blank">Povo</a>) i più attenti di voi avranno notato una 
+              tondeggiante struttura color panna vicino al campo da calcio. E' il Cinema Teatro Concordia,
+              piccolo teatro con schermo. <br/> 
+              Una breve chiacchierata col <a href="https://www.treccani.it/vocabolario/custode/" target="_blank">custode</a> 
+              mi ha confermato essere uno spazio che, sopratutto nell'ultimo periodo, viene utilizzato giusto 
+              una manciata di volte all'anno, nonostante la proposta di alcune iniziative tipo cineforum.<br/><br/>  
+              
+              È sì un peccato ma anche una golosa occasione per appassionati di cinema, perché senza troppe 
+              spese è possibile organizzare alcune proiezioni di film che difficilmente avremmo altrimenti 
+              l'occasione di vedere sul <a href="" target="_blank">grande schermo</a>.<br/><br/>
+
+              In coda a questo ragionamento ci piacerebbe dunque come Associazione Kazoo APS proporvi una 
+              carrellata di titoli sostanziosi che possano essere una modesta immersione nel cinema italiano 
+              degli anni '60 e '70.
+              L'invito è rivolto ad appassionati ed appassionate, con l'idea che possa anche però 
+              diventare la giusta occasione per coinvolgere amici e amiche, per spronarli ed avvicinarli 
+              ad una stagione cinematografica senza data di scadenza.<br/><br/>
+
+              Questo sito ha lo scopo di sondare il vostro interesse per l'iniziativa, vi proponiamo un primo 
+              film e, raccolte un numero sufficiente di adesioni, vi comunicheremo una data per la proiezione.<br/>
+              Dovremmo riuscire a coprire i costi mantenendo il prezzo del "biglietto" entro i 5€, che andrà 
+              diminuendo nel caso di una partecipazione numerosa.<br/>
+              Per ulteriori info accedete al gruppo Telegram o scrivete alla pagina @kazoo.inc su Instagram.<br/><br/>
+
+              Ciò che separa il dire e il fare in questo caso è il sapersi mettere d'accordo. Ci proviamo.
             </p>
             <Link to="/landing">
             <input
